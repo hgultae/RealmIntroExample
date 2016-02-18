d1c55ca079ef2872e55da0a68b0c8bab8666c15f
hurman <hurman.gul.theappexperts@gmail.com>
2016-02-18 14:55:26 +0000

    RealmIntroExample

---
+++ app/src/main/java/hg/com/realmintroexample/model/Cat.java
@@ -0,0 +1,15 @@
+package hg.com.realmintroexample.model;
+
+import io.realm.RealmObject;
+
+public class Cat extends RealmObject {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
